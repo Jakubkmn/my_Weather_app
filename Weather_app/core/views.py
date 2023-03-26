72a79cb292722cedd99b495b9e321122672b79ba
Kuba <jakubkmn12@gmail.com>
2023-03-26 13:46:43 +0200

    Writing views and changing templates

--- Weather_app/core/views.py
+++ Weather_app/core/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+import requests
 from django.http import HttpResponse
+from .models import City
 
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Welcome to Weather App</h1>')
+    return render(request, 'home.html')
+
+def info(request):
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+    return render(request, 'info.html')
